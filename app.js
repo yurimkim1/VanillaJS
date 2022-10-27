de5d253d859652f12caa3e019299168b7ffb339d
김유림 <yurim.kim@mirero.co.kr>
2022-10-27 10:06:03 +0900

    2회차 js 문법

---
+++ app.js
@@ -0,0 +1,33 @@
+// # chapter 2. 자바스크립트
+// HTML 파일은 브라우저에서 열수있고, HTML 파일을 통해 CSS와 자바스크립트를 가져온다.
+// 고로 index.html 파일만 브라우저에서 열면된다
+
+// 출력 명령어
+console.log(54541);
+console.log("asdassad");
+console.log('sdassad');  // 작은따옴표 사용도 가능
+
+// 변수 선언: const, let 
+let a = 5;
+const myVariableName = "javascript"; // 카멜표기 사용
+
+console.log(a + b);
+
+// var도 사용가능 
+// 재선언을 했는데 에러가 나오지 않고 각기 다른 값이 출력됨
+// 보완된 변수 선언 방식 let, const
+var name = 'js'
+console.log(name)
+
+var name = 'javascript'
+console.log(name)
+// 기본적으로 const, 필요할 때만 let, var는 쓰지말자
+
+// 데이터 타입
+number
+string
+boolean
+null과 undefined
+// 차이점 
+// null 값이 정해지지 않은 것
+// undefined란 타입이 정해지지 않은 것
